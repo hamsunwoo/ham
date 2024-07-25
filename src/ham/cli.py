a3319b378dade3548badfe43537b318e1fc9ef8e
sunwoo <summerham22@gmail.com>
2024-07-25 14:51:03 +0900

    add notebook

--- src/ham/cli.py
+++ src/ham/cli.py
@@ -28,10 +28,8 @@ def cmd():
             print(f"-d => {args.dt}")
             #TODO 특정 날짜의 명령어 TOP N
         else:
-            print("TODO - add error message")
             parser.error("-t 옵션은 -d 옵션과 함께 사용하시오!")
     else:
-        #TODO - 사용법을 출력한다 
         parser.print_help()
     
 
